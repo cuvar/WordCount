17125c08c915214e02016caa7e64466ae66af160
Luca Müller <luca.mueller01@gmx.de>
2020-04-18 15:36:47 +0200

    Corrected layout

--- main.py
+++ main.py
@@ -4,11 +4,25 @@
 
 import tkinter as tk
 
+def count():
+    
+    frequency_words = "Text"
+    
+    textin= inputText.get("1.0","end-1c")
+    print(textin)
+
+
+    #show in window
+    msg = tk.Message(root, text = frequency_words, width=250)
+    msg.pack()
+    msg.place(x=400, y=50)
+
 
 def initGui():
-    w = 600 # width for the Tk root
-    h = 500 # height for the Tk root
+    w = 700 # width for the Tk root
+    h = 550 # height for the Tk root
 
+    global root
     root = tk.Tk()
 
     ws = root.winfo_screenwidth() # width of the screen
@@ -22,18 +36,25 @@ def initGui():
     root.resizable(0, 0)
     #root.configure(bg='#9EA8B1')
 
+    #in title
     inTitle = tk.Label(text="Input Text")
-    inTitle.place(x=100, y=20)
-
-    outTitle = tk.Label(text="Quantity of words")
-    outTitle.place(x=400, y=20)
-
-    inputText = tk.Text(root, width=80, height=20)
+    inTitle.place(x=150, y=20)
+    
+    #text area
+    global inputText
+    inputText = tk.Text(root, width=40, height=30)
     inputText.place(x=20, y=50)
 
-    #input text
-    #click button
-    #output text
+    #button
+    but = tk.Button(root, text="Count", command=count)
+    but.pack()
+    but.place(x=325, y=20)
+
+    #out title
+    outTitle = tk.Label(text="Frequency of words")
+    outTitle.place(x=475, y=20)
+
+    
 
     root.mainloop()
 
